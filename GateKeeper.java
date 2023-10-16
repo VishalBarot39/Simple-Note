72c506fc35ef02202ad11e727e8f9663dd9d0c69
VishalBarot39 <145924119+VishalBarot39@users.noreply.github.com>
2023-10-16 20:18:36 +0530

    GateKeeper.java

--- GateKeeper.java
+++ GateKeeper.java
@@ -16,116 +16,58 @@ import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
-
-
 @WebFilter(filterName = "GateKeeper", urlPatterns = {"/*"},
-
            dispatcherTypes = {DispatcherType.REQUEST})
-
 public class GateKeeper implements Filter {
-
-    
-
     private FilterConfig filterConfig = null;
-
-    
-
     public GateKeeper() {
-
     }    
 
-    
 
     public void doFilter(ServletRequest request, ServletResponse response,
-
             FilterChain chain)
-
             throws IOException, ServletException {
-
-        
-
         HttpSession session;
-
         session = ((HttpServletRequest)request).getSession(false);
-
         String servletName =((HttpServletRequest)request).
-
                               getServletPath();
-
         if(session == null && !servletName.equals("/Login")){
-
             request.setAttribute("redirect", servletName.substring(1));
-
             request.getRequestDispatcher("Login").
-
                     forward(request, response);
-
         }
-
         if(
-
                 session != null && 
-
                 session.getAttribute("username") == null && 
-
                 !servletName.equals("/Login") && 
-
                 !servletName.equals("/Authenticator")
-
-          )
-
+            )
         {
-
                 request.setAttribute("redirect",
-
                         servletName.substring(1));
-
                 request.getRequestDispatcher("Login")
-
                         .forward(request, response);
-
         }   
 
         try {
-
             chain.doFilter(request, response);
-
         } catch (Throwable t) {
-
         }
-
     }
-
-
 
     public FilterConfig getFilterConfig() {
-
         return (this.filterConfig);
-
     }
-
-
 
     public void setFilterConfig(FilterConfig filterConfig) {
-
         this.filterConfig = filterConfig;
-
     }
-
-
 
     public void init(FilterConfig filterConfig) {        
-
         this.filterConfig = filterConfig;
-
     }
 
-
-
     @Override
-
     public void destroy() {
-
     }   
-
 }
